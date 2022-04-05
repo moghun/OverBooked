5555b2f47f07a86ed1e2f8a1c9f98fa6a187f780
Muhammed Orhun Gale <89805772+moghun@users.noreply.github.com>
2022-04-05 18:21:10 +0300

    product list init

--- client/src/pages/product-view/ProductPage.js
+++ client/src/pages/product-view/ProductPage.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
-const DetailsThumb = ({products}) => {
+const DetailsThumb = () => {
+
+    const [products, setProduct] = useState(
+        [{
+        "_id": "1",
+        "title": "Ay Zalim Bir Sevgilidir",
+        "src": "http://www.ithaki.com.tr/wp-content/uploads/2017/06/Ay-Zalim-Bir-Sevgilidir.jpg",
+        "price": "23.99 TL",
+        "content": "Ay’ı Dünya’dan kontrol eden Otorite’ye karşı, yalnızca mahkûm ve sürgünlerin gönderildiği ceza kolonisine dönüşmüş Ay’daki isyanın ve devrimin öyküsü bu.",
+        "count": 1,
+        "author": "Robert A. Heinlein",
+        "publisher": "İthaki Yayınları",
+        "amount": 10
+      }]);
+      
     return (
         <div className = "app">
             <div className="details" key={products.id}>
