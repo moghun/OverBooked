8cbcc4ceb3d5d5cccfe6571ce4aaf322ef31d3d3
Minot1 <dincer.taner123@gmail.com>
2022-04-07 16:09:41 +0300

    created BookCard component

--- client/src/pages/product-view/Home.js
+++ client/src/pages/product-view/Home.js
@@ -5,6 +5,8 @@ import "../../components/Navigation_Bar/NavigationBar.css";
 // import {Link} from "react-router-dom";
 
 import axios from "axios";
+import BookCard from "../../components/BookCard";
+import { Grid } from "@mui/material";
 const HomePage = () => {
   const [currentSlide, setCurrentSlide] = useState(1);
   const slideLength = sliderData.length;
@@ -23,41 +25,38 @@ const HomePage = () => {
     console.log("prev");
   };
 
-  const moveDot = index => {
-    setCurrentSlide(index)
-}
+  const moveDot = (index) => {
+    setCurrentSlide(index);
+  };
 
-function auto() {
-  slideInterval = setInterval(nextSlide, intervalTime);
-}
+  function auto() {
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
 
-useEffect(() => {
-  setCurrentSlide(0);
-}, []);
+  useEffect(() => {
+    setCurrentSlide(0);
+  }, []);
 
-
-const getBooks = async () => {
-  
-  await axios.get("http://localhost:5001/api/users")
-    .then(res => {
-      console.log("aa");
-      console.log(res.data);
-      const data = res.data;
-      let bookList = [];
-/*         data.forEach(uni => {
+  const getBooks = async () => {
+    await axios
+      .get("http://localhost:5001/api/users")
+      .then((res) => {
+        console.log("aa");
+        console.log(res.data);
+        const data = res.data;
+        let bookList = [];
+        /*         data.forEach(uni => {
         courseList = courseList.concat(...uni.courses)
       }); */
 
-      console.log(bookList);
+        console.log(bookList);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    }).catch(err => console.log(err))
-}
-
-
-useEffect(() => {
-  getBooks();
-})
-
+  useEffect(() => {
+    getBooks();
+  });
 
   useEffect(() => {
     if (autoScroll) {
@@ -67,39 +66,50 @@ useEffect(() => {
   }, [currentSlide]);
 
   return (
-    <div className="slider">
-      <AiOutlineArrowLeft className="arrow prev" onClick={prevSlide} />
-      <AiOutlineArrowRight className="arrow next" onClick={nextSlide} />
-      {sliderData.map((slide, index) => {
-        return (
-          <div
-            className={index === currentSlide ? "slide current" : "slide"}
-            key={index}
-          >
-            {index === currentSlide && (
-              <div>
-                <img src={slide.image} alt="slide" className="image" />
-                <div className="content">
-                  <h2>{slide.heading}</h2>
-                  <p>{slide.desc}</p>
-                  <hr />
-                  <form action="\productpage" method="get"> 
-                  <button className="--btn --btn-primary">Go To This Product </button>
-                  </form>
+    <div>
+      <div className="slider">
+        <AiOutlineArrowLeft className="arrow prev" onClick={prevSlide} />
+        <AiOutlineArrowRight className="arrow next" onClick={nextSlide} />
+        {sliderData.map((slide, index) => {
+          return (
+            <div
+              className={index === currentSlide ? "slide current" : "slide"}
+              key={index}
+            >
+              {index === currentSlide && (
+                <div>
+                  <img src={slide.image} alt="slide" className="image" />
+                  <div className="content">
+                    <h2>{slide.heading}</h2>
+                    <p>{slide.desc}</p>
+                    <hr />
+                    <form action="\productpage" method="get">
+                      <button className="--btn --btn-primary">
+                        Go To This Product{" "}
+                      </button>
+                    </form>
+                  </div>
                 </div>
-              </div>
-            )}
-          </div>
-        );
-      })}
-      <div className="container-dots">
-        {Array.from({length: 3}).map((item, index) => (
-          <div 
-          onClick={() => moveDot(index + 1)}
-          className={currentSlide === index + 1 ? "dot active" : "dot"}
-          ></div>
-        ))}
+              )}
+            </div>
+          );
+        })}
+        <div className="container-dots">
+          {Array.from({ length: 3 }).map((item, index) => (
+            <div
+              onClick={() => moveDot(index + 1)}
+              className={currentSlide === index + 1 ? "dot active" : "dot"}
+            ></div>
+          ))}
+        </div>
       </div>
+      <Grid container direction="row" justifyContent="space-around">
+        <BookCard name="deneme1" author="zaa" imgurl="/animalfarm.jpg" publisher="yayınevi" price="87.99 TL" score="3.7/5.0"></BookCard>
+        <BookCard name="deneme2" author="zaa" imgurl="/animalfarm.jpg" publisher="yayınevi" price="87.99 TL" score="3.7/5.0"></BookCard>
+        <BookCard name="deneme3" author="zaa" imgurl="/animalfarm.jpg" publisher="yayınevi" price="87.99 TL" score="3.7/5.0"></BookCard>
+        <BookCard name="deneme4" author="zaa" imgurl="/animalfarm.jpg" publisher="yayınevi" price="87.99 TL" score="3.7/5.0"></BookCard>
+        <BookCard name="deneme5" author="zaa" imgurl="/animalfarm.jpg" publisher="yayınevi" price="87.99 TL" score="3.7/5.0"></BookCard>
+      </Grid>
     </div>
   );
 };
