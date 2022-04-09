226de06957543dbd804946945e193481f499d732
Minot1 <dincer.taner123@gmail.com>
2022-04-09 18:19:00 +0300

    update SearchPage: added main grid structure

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,14 +1,35 @@
-import { Grid } from '@mui/material'
-import React from 'react'
+import { Box, Button, Card, Grid } from "@mui/material";
+import React from "react";
+import BookCard from "../components/BookCard";
 
 const SearchPage = () => {
   return (
     <div>
-      <Grid container>
-        {/* map function to Grid items */}
+      <Grid
+        container
+        padding="40px"
+        justifyContent="space-around"
+      > {/* Main grid */}
+        <Grid item xs={3} justifyContent="space-around" display="flex">
+          {/* Filtering card part (left) */}
+          <Card direction="column" justifyContent="center" display="flex" style={{ minWidth: "300px" }}>
+            <Box justifyContent="center" display="flex">deneme</Box>
+          </Card>
+        </Grid>
+        <Grid item container xs={9} justifyContent="space-around" display="flex">
+          {/* Results of search and outcome of filters (right) */}
+        <BookCard name="deneme1" author="author1" imgurl="/images/animalfarm.jpg" publisher="yayınevi" price="87.99 TL" score="3.7/5.0"></BookCard>
+        <BookCard name="deneme1" author="author1" imgurl="/images/animalfarm.jpg" publisher="yayınevi" price="87.99 TL" score="3.7/5.0"></BookCard>
+        <BookCard name="deneme1" author="author1" imgurl="/images/animalfarm.jpg" publisher="yayınevi" price="87.99 TL" score="3.7/5.0"></BookCard>
+        <BookCard name="deneme1" author="author1" imgurl="/images/animalfarm.jpg" publisher="yayınevi" price="87.99 TL" score="3.7/5.0"></BookCard>
+        <BookCard name="deneme1" author="author1" imgurl="/images/animalfarm.jpg" publisher="yayınevi" price="87.99 TL" score="3.7/5.0"></BookCard>
+        <BookCard name="deneme1" author="author1" imgurl="/images/animalfarm.jpg" publisher="yayınevi" price="87.99 TL" score="3.7/5.0"></BookCard>
+        <BookCard name="deneme1" author="author1" imgurl="/images/animalfarm.jpg" publisher="yayınevi" price="87.99 TL" score="3.7/5.0"></BookCard>
+        </Grid>
+        {/* TODO: Create a map function to Grid items */}
       </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
