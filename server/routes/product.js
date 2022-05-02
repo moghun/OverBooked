a4e0d191c436a40a576abb5603241b528c36c25a
Muhammed Orhun Gale <89805772+moghun@users.noreply.github.com>
2022-05-03 02:19:29 +0300

    product pull functionality added

--- server/routes/product.js
+++ server/routes/product.js
@@ -56,21 +56,96 @@ router.get("/find/:id", async (req, res) => {
   }
 });
 
-//GET ALL PRODUCTS
-router.get("/", async (req, res) => {
-  const qNew = req.query.new;
-  const qCategory = req.query.category;
+
+//GET SEARCHED WORD - SEARCH BAR FUNCTIONALITY
+router.get("/find", async (req, res) => {
+  const q = req.query.q;
   try {
     let products;
+    let products_author;
+    let products_cat;
+//  let products_subcat;
 
-    if (qNew) {
-      products = await Product.find().sort({ createdAt: -1 }).limit(1);
+    products = await Product.find(
+      {
+        name: {
+          $regex: new RegExp(q, 'i')
+        }
+      }
+    );
+
+    products_author = await Product.find(
+      {
+        author: {
+          $regex: new RegExp(q, 'i')
+        }
+      }
+    );
+
+    products_cat = await Product.find(
+      {
+        category: {
+          $regex: new RegExp(q, 'i')
+        }
+      }
+    );
+
+    //Subcategory search -- needs sophisticated query since it requires to search with regex
+   /*
+    products_subcat = await Product.aggregate(
+      {
+        $match: {
+          "subcategories": 
+          { 
+            $regex: new RegExp(q, 'i')
+          }
+        }
+      }
+    );*/
+    
+    products = products.concat(products_author);
+    products = products.concat(products_cat);
+//  products = products.concat(products_subcat);
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+//GET ALL PRODUCTS
+//DEFINE FILTER W/ SPECIFIC QUERY TYPE
+router.get("/", async (req, res) => {
+  const qTop = req.query.top;
+  const qSale = (req.query.sale === "true");
+  const qCategory = req.query.category;
+  const qSubCategory = req.query.subcategory;
+  try {
+    let products;
+    
+    if (qTop) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(10);
+    } else if (qSale) {
+      products = await Product.find({
+        sale: {
+          $eq: qSale,
+        }
+      
+      });
     } else if (qCategory) {
       products = await Product.find({
-        categories: {
+        category: {
           $in: [qCategory],
         },
       });
+    } else if (qSubCategory) {
+        products = await Product.find({
+          subcategories: {
+            $in: [qSubCategory],
+          },
+        });
     } else {
       products = await Product.find();
     }
