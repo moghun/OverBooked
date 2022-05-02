f87a71f652a3016d92bbde6a31dc7742208a5c7e
Muhammed Orhun Gale <89805772+moghun@users.noreply.github.com>
2022-05-02 22:29:43 +0300

    fix-models

--- server/models/user_dbmod.js
+++ server/models/user_dbmod.js
@@ -2,23 +2,25 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    mail: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
     name: { type: String, required: false },
     surname: { type: String, required: false },
+   
     fav_products: {type: Array, required: false},
     cart: {type: Array, required: false},
     orders: {type: Array, required: false},
     
+
     hasStore: {
       type: Boolean,
       default: false,
     },
+    store_name: { type: String, required: false },
     rating: {type: Number, required: false},
     current_products: {type: Array, required: false},
     sold_products: {type: Array, required: false},
-    username: { type: String, required: true },
-
   },
   { timestamps: true }
 );
