b86bda8f870fee66b7d743ebe4326e7c7bb14e90
Muhammed Orhun Gale <89805772+moghun@users.noreply.github.com>
2022-05-05 23:12:59 +0300

    authentication token bug fixed

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -41,7 +41,7 @@ router.post("/login", async (req, res) => {
     const accessToken = jwt.sign(
       {
         id: user._id,
-        isAdmin: user.isAdmin,
+        user_role: user.user_role,
       },
       process.env.JWT_SEC,
       {expiresIn:"3d"}
