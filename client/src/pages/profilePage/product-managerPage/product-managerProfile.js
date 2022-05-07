6c4dd2a95a3fcd316445adc2f8779fd0904d09da
Muhammed Orhun Gale <89805772+moghun@users.noreply.github.com>
2022-05-07 11:57:56 +0300

    minor identation fix

--- client/src/pages/profilePage/product-managerPage/product-managerProfile.js
+++ client/src/pages/profilePage/product-managerPage/product-managerProfile.js
@@ -46,18 +46,16 @@ const disapproveComment = async (product_id, comment_no) => {
 const comments = getComments();
 const unappCommennts = getUnapprovedComments(comments);
 
-
 //Assume that products cards are visualized here, product-manager now can select a comment to approve
 
 //EXAMPLE APPROVE - DISAPPROVE COMMENT - user clicked to a comment for approval
 const demoExecution = () =>{
     //Assume that s/he clicked to the first unapproved comment for approving
-    const commentToApprove = unappCommennts[0];
-
-    approveComment(commentToApprove.product_id, commentToApprove.initCommit);
+    const commentToApprove1 = unappCommennts[0];
+    approveComment(commentToApprove1.product_id, commentToApprove1.initCommit);
 
 
     //Assume that s/he clicked to the second unapproved comment for disapproving
-    const commentToDisapprove = unappCommennts[1];
-    disapproveComment(commentToDisapprove.product_id, commentToDisapprove.initCommit)
+    const commentToDisapprove2 = unappCommennts[1];
+    disapproveComment(commentToDisapprove2.product_id, commentToDisapprove2.initCommit)
 }
