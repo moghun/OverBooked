6edc8d124995a6b48b9273ae26b5f434482baacb
melihg <melihg@sabanciuniv.edu>
2022-05-07 23:34:22 +0300

    still bugged async

--- client/src/pages/profilePage/product-managerPage/product-managerProfile.js
+++ client/src/pages/profilePage/product-managerPage/product-managerProfile.js
@@ -1,12 +1,11 @@
-import { padding } from '@mui/system';
-import React, { useState, useEffect } from 'react';
-import {FaStar} from 'react-icons/fa';
-import { useLocation } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { padding } from "@mui/system";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
-import '../product-managerPage/CommentApprove.css';
+import "../product-managerPage/CommentApprove.css";
 /*
+
+
 //ALL COMMENTS TO BE APPROVE
 const getComments = async () => {
     try{
@@ -59,81 +58,140 @@ const demoExecution = () =>{
     disapproveComment(commentToDisapprove2.product_id, commentToDisapprove2.initCommit)
 }
 */
-const CommentApprove = () => {
-    const currUser = useSelector((state) => state.user.currentUser);
 
-    console.log(currUser);
-    const getComments = async () => {
-        try{
-            const res = await axios.get("http://localhost:5001/api/products/commentApproval", {headers: {token: ("Bearer "+ currUser.accessToken)}});
-            return res.data;
-        } catch (err){}
-    }
+function ProductManagerAPI() {
+  const CurrUser = useSelector((state) => state.user.currentUser);
 
-    
-    const getUnapprovedComments = (productsWithComments) => {
-        let returnArr = []
+  const getComments = async (CurrUser) => {
+    try {
+      const res = await axios.get(
+        "http://localhost:5001/api/products/commentApproval",
+        { headers: { token: "Bearer " + CurrUser.accessToken } }
+      );
+      return res.data;
+    } catch (err) {}
+  };
 
-
-        console.log(productsWithComments)
-
-        for (var product in productsWithComments)
-        {
-
-            for (var comment in product.comments)
-            {
-                if(comment.isApproved === false)
-                {
-                    returnArr.push({product_id: product._id, initCommit: comment})
-                }
-            }
+  const getUnapprovedComments = (productsWithComments) => {
+    let returnArr = [];
+    for (let product in productsWithComments) {
+      console.log(productsWithComments[product]);
+      for (let comment in productsWithComments[product].comments) {
+        if (
+          productsWithComments[product].comments[comment].isApproved === false
+        ) {
+          returnArr.push({
+            product_id: productsWithComments[product]._id,
+            initCommit: productsWithComments[product].comments[comment].comment,
+          });
         }
-        return returnArr;
+      }
     }
+    return returnArr;
+  };
 
+  const comments = getComments(CurrUser);
+  console.log(typeof comments);
+  const unappCommennts = getUnapprovedComments(comments);
+  console.log(typeof unappCommennts);
+
+  const CommentApprove = () => {
     const approveComment = async (product_id, comment_no) => {
-        try{
-            const res = await axios.put("http://localhost:5001/api/products/commentApproval/" + product_id + "/" + comment_no, {headers: {token: ("Bearer "+ currUser.accessToken)}});
-        } catch (err){}   
-    }
+      try {
+        const res = await axios.put(
+          "http://localhost:5001/api/products/commentApproval/" +
+            product_id +
+            "/" +
+            comment_no,
+          { headers: { token: "Bearer " + CurrUser.accessToken } }
+        );
+      } catch (err) {}
+    };
 
     const disapproveComment = async (product_id, comment_no) => {
-        try{
-            const res = await axios.put("http://localhost:5001/api/products/commentApproval/delete/" + product_id + "/" + comment_no, {headers: {token: ("Bearer "+ currUser.accessToken)}});
-        } catch (err){}   
-    }
-
-    const comments = getComments().then(result => result.data);
-    const unappCommennts = getUnapprovedComments(comments);
-    console.log(unappCommennts);
+      try {
+        const res = await axios.put(
+          "http://localhost:5001/api/products/commentApproval/delete/" +
+            product_id +
+            "/" +
+            comment_no,
+          { headers: { token: "Bearer " + CurrUser.accessToken } }
+        );
+      } catch (err) {}
+    };
 
     return (
-        <div className='approval-holder'>
-  
-        <div className='Row'>
-    
-          <div className='approval-container'>
+      <div className="approval-holder">
+        <div className="Row">
+          <div className="approval-container">
             <div>
-    
-              {unappCommennts.length === 0 
-              ? <h1 style={{padding: "50px",textAlign:'center'}}>You have no comment waiting approval.</h1>
-              
-              :
-              unappCommennts.map((item)=>{
-                    return (
-                      <div className="approval-column"style={{padding: '10px', margin:'20px'}}>
-                        <h style={{marginLeft: '25px', fontWeight:'bold', marginRight:'20px'}}>Product ID:{item.product_id}</h>
-                        <h style={{marginLeft: '50px',marginRight:-20, fontWeight:'bold'}}>Comment:{item.initCommit} </h>
-                        <input type = "submit" onClick={approveComment(item.product_id, item.initCommit)} value = "Approve"  style={{backgroundColor: 'lightgreen', marginLeft: '300px',width:"100px",borderRadius:'5px'}}/>
-                        <input type = "submit" onClick={disapproveComment(item.product_id, item.initCommit)} value = "Disapprove"  style={{backgroundColor: 'red',marginLeft: '50px', width:"100px",borderRadius:'5px'}}/>
-                      </div>
-                  );})}
-    
+              {console.log(unappCommennts)}
+              {unappCommennts.length === 0 ? (
+                <h1 style={{ padding: "50px", textAlign: "center" }}>
+                  You have no comment waiting approval.
+                </h1>
+              ) : (
+                unappCommennts.forEach((item) => {
+                  {
+                    console.log(item);
+                  }
+                  <div
+                    className="approval-column"
+                    style={{ padding: "10px", margin: "20px" }}
+                  >
+                    <h
+                      style={{
+                        marginLeft: "25px",
+                        fontWeight: "bold",
+                        marginRight: "20px",
+                      }}
+                    >
+                      Product ID:{item.product_id}
+                    </h>
+                    <h
+                      style={{
+                        marginLeft: "50px",
+                        marginRight: -20,
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Comment:{item.initCommit}{" "}
+                    </h>
+
+                    <input
+                      type="submit"
+                      onClick={approveComment(item.product_id, item.initCommit)}
+                      value="Approve"
+                      style={{
+                        backgroundColor: "lightgreen",
+                        marginLeft: "300px",
+                        width: "100px",
+                        borderRadius: "5px",
+                      }}
+                    />
+                    <input
+                      type="submit"
+                      onClick={disapproveComment(
+                        item.product_id,
+                        item.initCommit
+                      )}
+                      value="Disapprove"
+                      style={{
+                        backgroundColor: "red",
+                        marginLeft: "50px",
+                        width: "100px",
+                        borderRadius: "5px",
+                      }}
+                    />
+                  </div>;
+                })
+              )}
             </div>
           </div>
         </div>
-        </div>
-      )
+      </div>
+    );
+  };
+  CommentApprove();
 }
-
-export default CommentApprove;
+export default ProductManagerAPI;
