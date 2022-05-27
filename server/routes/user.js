8986635d782d05d560805dcdf365158d5b3f4125
Muhammed Orhun Gale <89805772+moghun@users.noreply.github.com>
2022-05-28 02:53:48 +0300

    user_id can be reachable to label comments

--- server/routes/user.js
+++ server/routes/user.js
@@ -200,4 +200,15 @@ router.put("/invoice/:id", verifyToken, async (req, res) => {
   }
 });
 
+//GET USERNAME
+router.get("/getUsername/:id", verifyTokenOrManager, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { username, ...others } = user._doc;
+    res.status(200).json(username);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
