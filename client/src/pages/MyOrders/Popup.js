a6a77457be6aed0267e0fd40034affea73ddf94a
Gürkan Talha Soylu (Student) <gurkansoylu@sabanciuniv.edu>
2022-05-28 15:16:14 +0300

    id box added, hover effects added

--- client/src/pages/MyOrders/Popup.js
+++ client/src/pages/MyOrders/Popup.js
@@ -5,18 +5,22 @@ import { useState } from "react";
 const Popup = props => {
 
     const [text, setText] = useState(null);
+    const [id, setId] = useState(null);
+
+
 
     function printText(){
-        alert(text+ " " + props.id);
+        alert(text+ " " + id);
     }
 
   return (
     <div className="popup-box">
       <div className="box">
             <span className="close-icon" onClick={props.handleClose}>x</span>
-            <h style={{fontSize:'20px', fontFamily:'OpenSans', marginLeft:'20%'}}>Please explain your cancelation reason</h>
+            <h style={{color:'#FAFAFA', fontSize:'20px', fontFamily:'OpenSans', marginLeft:'20%'}}>Please explain your cancelation reason</h>
 
-            <div><textarea onChange={(e)=>{ setText(e.target.value)}} rows="4" cols="50" style={{resize:'none', width:'100%', outline:'none', borderRadius:'10px'}}></textarea></div>
+            <div><textarea placeholder=" Type your order id..." onChange={(e)=>{ setId(e.target.value)}} rows="1" cols="50" style={{resize:'none', width:'100%', outline:'none',borderRadius:'10px'}}></textarea></div>
+            <div><textarea placeholder=" Type your reasoning..." onChange={(e)=>{ setText(e.target.value)}} rows="4" cols="50" style={{resize:'none', width:'100%', outline:'none', borderRadius:'10px'}}></textarea></div>
 
             <button onClick={printText} style={{
             fontFamily:'OpenSans', 
@@ -25,6 +29,7 @@ const Popup = props => {
             borderWidth:'1px',
             borderRadius:'5px',
             width:'50px',
+            fontSize:'20px',
             backgroundColor:'white'
             }}>Send</button>
         </div>
