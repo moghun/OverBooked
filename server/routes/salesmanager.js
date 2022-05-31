76c9fe90ba31971b8c954a9a5684c59b757ea281
Tolga Aydın <tolgaaydin@sabanciuniv.edu>
2022-05-31 19:08:38 +0300

    Revenue graph.

--- server/routes/salesmanager.js
+++ server/routes/salesmanager.js
@@ -8,6 +8,7 @@ const {
   verifyTokenAndManager,
   verifyTokenAndProductManager,  
 } = require("./verifyToken");
+const { mapReduce } = require("../models/user_dbmod");
 
 const router = require("express").Router();
 
@@ -25,9 +26,16 @@ router.get("/betweendates", async (req, res) => {
         const from = new Date(start_date);
         const to = new Date(end_date);
         
-        const orders = Order.find({date:{$gt:from,$lt:to}});
+        const orders = await Order.find({date:{$gte:from,$lt:to}});
+        
+        const cost_arr = orders.map((order) => order.cost);
 
-        return res.status(200).json(orders);
+        var sum = 0;
+        for(let i = 0; i < cost_arr.length;i++){
+          sum += cost_arr[i];
+        }
+
+        return res.status(200).json(sum);
 
     } catch (err) {
       return res.status(500).json(err);
