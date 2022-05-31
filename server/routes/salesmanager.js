408b0cd2c0413d68368bf7dea3109e155f35f12f
Tolga Aydın <tolgaaydin@sabanciuniv.edu>
2022-05-31 15:03:23 +0300

    Graph improvements

---
+++ server/routes/salesmanager.js
@@ -0,0 +1,35 @@
+const Product = require("../models/product_dbmod");
+const Order = require("../models/order_dbmod");
+const User = require("../models/user_dbmod");
+const {
+  verifyToken,
+  verifyTokenAndUser,
+  verifyTokenOrManager,
+  verifyTokenAndManager,
+  verifyTokenAndProductManager,  
+} = require("./verifyToken");
+
+const router = require("express").Router();
+
+
+//GET INVOICE BETWEEN THE DATES
+router.get("/betweendates", async (req, res) => {
+    try {
+        const start_date = req.body.start_date;
+        const end_date = req.body.end_date;
+        if(!start_date||!end_date){
+            return res.status(403).json("Dates can not be empty!");
+        }
+        const from = new Date(start_date);
+        const to = new Date(end_date);
+
+        const orders = Order.find({date:{$gte:ISODate(from),$lt:ISODate(to)}})
+
+        return res.status(200).json(orders);
+
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  });
+
+  module.exports = router;
--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const productRoute = require("./routes/product");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
 const productmanRoute = require("./routes/productmanager");
+const salesmanRoute = require("./routes/salesmanager")
 const cors = require("cors");
 const { options } = require("./routes/auth");
 
@@ -30,6 +31,7 @@ app.use("/api/products", productRoute);
 app.use("/api/orders",orderRoute);
 app.use("/api/checkout", stripeRoute);
 app.use("/api/productmanager",productmanRoute);
+app.use("/api/salesmanager",salesmanRoute);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log("Server is running!");
