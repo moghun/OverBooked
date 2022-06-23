7b8e30a2dec9bcee092efcb285b19b67703c3105
Muhammed Orhun Gale <89805772+moghun@users.noreply.github.com>
2022-06-23 23:07:31 +0300

    minor change

--- server/models/product_dbmod.js
+++ server/models/product_dbmod.js
@@ -2,22 +2,22 @@ const mongoose = require("mongoose");
 
 const productsSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    cost: { type: Number, required: true },
-    amount: { type: Number, required: true },
-    publisher: { type: String, required: true },
-    img: { type: String, required: false , default: ""},
-    description: { type: String, required: true, default: ""},
-    warranty: { type: Number, required: false, default: -1},
-    author: { type: String, required: true },
+    name: { type: String, required: false },
+    cost: { type: Number, required: false },
+    amount: { type: Number, required: false },
+    publisher: { type: String, required: false },
+    img: { type: String, required: false, default: "" },
+    description: { type: String, required: false, default: "" },
+    warranty: { type: Number, required: false, default: -1 },
+    author: { type: String, required: false },
 
-    category: { type: String, required: true },
+    category: { type: String, required: false },
     subcategories: { type: Array, required: false },
 
-    sale: { type: Boolean, required: false,  default: false},
-    before_sale_price: { type: Number, required: false, default: -1},
-    rating: { type: Array, required: false, default: []},
-    comments: { type: Array, required: false, default:[] },
+    sale: { type: Boolean, required: false, default: false },
+    before_sale_price: { type: Number, required: false, default: -1 },
+    rating: { type: Array, required: false, default: [] },
+    comments: { type: Array, required: false, default: [] },
   },
   { timestamps: true }
 );
